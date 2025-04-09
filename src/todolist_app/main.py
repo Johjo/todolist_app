6d87c7c6d5b7bc86fa34d3f41bbf43247e030959
Jonathan <johjo@ytreza.org>
2025-04-09 23:28:25 +0200

    feat: return uuid when create todolist

--- src/todolist_app/main.py
+++ src/todolist_app/main.py
@@ -1,13 +1,14 @@
 import os
 from abc import abstractmethod
+from uuid import UUID, uuid4
 
-from bottle import Bottle, TEMPLATE_PATH, template, request
+from bottle import Bottle, TEMPLATE_PATH, template, request #type: ignore
 from dotenv import load_dotenv
 
 
 class ControllerPort:
     @abstractmethod
-    def create_todolist(self, name: str) -> None:
+    def create_todolist(self, name: str) -> UUID:
         pass
 
 
@@ -16,8 +17,6 @@ def start_app(controller: ControllerPort):
     views_dir = os.path.join(current_dir, 'views')
     TEMPLATE_PATH.insert(0, views_dir)
     app = Bottle()
-
-    # Ajout du chemin `views` à TEMPLATE_PATH
 
     @app.route('/')
     def index():
@@ -38,9 +37,9 @@ port = os.getenv("PORT")
 
 
 class ControllerForDemo(ControllerPort):
-    def create_todolist(self, name: str) -> None:
+    def create_todolist(self, name: str) -> UUID:
         print(f"Nouvelle liste créée : {name}")
-
+        return uuid4()
 
 
 start_app(controller=ControllerForDemo()).run(host=host, port=port, reloader=True)
