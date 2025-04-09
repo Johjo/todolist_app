9cafc713ee55ae48d9a6a8530f48c94a94ac549d
Jonathan <johjo@ytreza.org>
2025-04-09 23:13:04 +0200

    refactor: Restructure main.py with app initialization function and environment loading

--- src/todolist_app/main.py
+++ src/todolist_app/main.py
@@ -1,19 +1,28 @@
 import os
 from bottle import Bottle, TEMPLATE_PATH, template
+from dotenv import load_dotenv
 
-app = Bottle()
+def start_app():
+    app = Bottle()
 
-# Ajout du chemin `views` à TEMPLATE_PATH
-CURRENT_DIR = os.path.dirname(__file__)
-VIEWS_DIR = os.path.join(CURRENT_DIR, 'views')
-TEMPLATE_PATH.insert(0, VIEWS_DIR)
+    # Ajout du chemin `views` à TEMPLATE_PATH
+    CURRENT_DIR = os.path.dirname(__file__)
+    VIEWS_DIR = os.path.join(CURRENT_DIR, 'views')
+    TEMPLATE_PATH.insert(0, VIEWS_DIR)
+
+
+    @app.route('/')
+    def index():
+        return template('index')
+
+    return app
+
+load_dotenv()
 
 host = os.getenv("HOST")
 port = os.getenv("PORT")
 
-@app.route('/')
-def index():
-    return template('index')
 
+app =start_app()
 app.run(host=host, port=port)
 
