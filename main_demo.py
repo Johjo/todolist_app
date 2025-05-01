986e4fe5e29e8ec318d2df589f0fb9c1a4de88b9
Jonathan <johjo@ytreza.org>
2025-05-01 06:38:37 +0200

    feat: use database

--- main_demo.py
+++ main_demo.py
@@ -10,5 +10,12 @@ load_dotenv()
 
 host = os.getenv("HOST")
 port = os.getenv("PORT")
+db_name = os.getenv("DB_NAME")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+db_host = os.getenv("DB_HOST")
+db_port = os.getenv("DB_PORT")
 
-start_app(controller=create_todolist_controller()).run(host=host, port=port, reloader=True, debug=True)
+start_app(controller=create_todolist_controller(db_name=db_name, db_user=db_user, db_host=db_host, db_port=db_port,
+                                                db_password=db_password)).run(host=host, port=port, reloader=True,
+                                                                              debug=True)
